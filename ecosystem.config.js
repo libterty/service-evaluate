ec293960bb83a534cb558432aaee8a5ebcd43206
李韻晟 <liyuncheng@MacBook-Pro.local>
2020-11-06 01:08:38 +0800

    feat: adding container method

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "service-evaluate",
+      script: "dist/src/main",
+      watch: true,
+      error_file: "/logs/service-evaluate.log",
+      out_file: "/logs/service-evaluate.log",
+      merge_logs: true,
+    },
+  ],
+};

2d623a6dd7716aa04581cf2c5a180e2bbc8ee407
李韻晟 <dreak603@gmail.com>
2020-11-13 17:57:34 +0800

    feat: adding create methods to controller and service layer

--- src/rates/rate.controller.ts
+++ src/rates/rate.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { IRate } from './rate.dto';
 import { Rate } from './rate.entity';
 import { RateService } from './rate.service';
 
@@ -9,5 +17,11 @@ export class RateController {
   @Get()
   getRates(): Promise<Rate[] | Error> {
     return this.rateService.getRates();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createRate(@Body() rateDto: IRate): Promise<Rate | Error> {
+    return this.rateService.createRate(rateDto);
   }
 }
--- src/rates/rate.service.ts
+++ src/rates/rate.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateWhenError } from 'libs/error';
+import { IRate } from './rate.dto';
 import { Rate } from './rate.entity';
 import { RateRepository } from './rate.repository';
 
@@ -8,12 +10,24 @@ export class RateService {
   constructor(
     @InjectRepository(RateRepository) private rateRepository: RateRepository,
   ) {}
+
+  /**
+   * @description Get Rates By Pagination
+   */
   async getRates(): Promise<Rate[] | Error> {
     try {
       const rates = await this.rateRepository.getRates();
       return rates;
     } catch (error) {
       throw new Error(error.message);
+    }
+  }
+
+  async createRate(rateDto: IRate): Promise<Rate | Error> {
+    try {
+      return await this.rateRepository.createRate(rateDto);
+    } catch (error) {
+      throw new CreateWhenError(error.message);
     }
   }
 }

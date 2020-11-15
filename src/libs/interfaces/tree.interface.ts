e840f42d8aa3396a3c2fe18d6b0f43266603f801
李韻晟 <dreak603@gmail.com>
2020-11-15 21:48:05 +0800

    feat: create tree interface

---
+++ src/libs/interfaces/tree.interface.ts
@@ -0,0 +1,18 @@
+export type TDimension = number | null;
+export type TKDNodeObj = any[];
+export type TMetrix = number;
+export type TPoint = any;
+
+export interface IObject {
+  [key: string]: any;
+}
+
+export interface IKDNode {
+  obj: TKDNodeObj;
+  dimension: number | null;
+  parent: IKDNode | null;
+  left: IKDNode | null;
+  right: IKDNode | null;
+}
+
+export interface IRoot extends IKDNode {}

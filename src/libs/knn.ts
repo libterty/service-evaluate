6126633e4a2c73b153bba29cef151b989fbf0131
李韻晟 <dreak603@gmail.com>
2020-11-15 16:06:42 +0800

    chore: update code origin

--- src/libs/knn.ts
+++ src/libs/knn.ts
@@ -1,25 +1,13 @@
-/**
- * MIT License
+/*
+ * Original code from:
  *
- * Copyright (c) 2018 Packt
+ * PacktPublishing/Hands-On-Machine-Learning-with-JavaScript
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * https://github.com/PacktPublishing/Hands-On-Machine-Learning-with-JavaScript
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * @auhtor https://github.com/PacktPublishing
+ * @author sayli2212
+ * @license MIT License <https://github.com/PacktPublishing/Hands-On-Machine-Learning-with-JavaScript/blob/master/LICENSE>
  */
 
 interface IMap {

24a9cbf40751e20422332fe3c71bd2688b907de4
李韻晟 <dreak603@gmail.com>
2020-11-28 14:42:14 +0800

    chore: changing orm config for pg

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -2,10 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from '../../config';
 
 export const ormConfig: TypeOrmModuleOptions = {
-  type: 'mongodb',
+  type: 'postgres',
   host: config.DB_SETTINGS.host,
   port: config.DB_SETTINGS.port,
+  username: config.DB_SETTINGS.username,
+  password: config.DB_SETTINGS.password,
   database: config.DB_SETTINGS.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: false,
+  migrations: [__dirname + './migration/*.ts'],
+  synchronize: true,
 };

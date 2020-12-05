04b67fba6004f7637ff8237f277bbcd4b65c71a5
李韻晟 <dreak603@gmail.com>
2020-12-05 17:09:02 +0800

    feat: adding db create script

---
+++ scripts/createdb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CREATE DATABASE lib
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;

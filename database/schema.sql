dad76997fbca6b3ab480ec1e2f47a941d4842a15
李韻晟 <dreak603@gmail.com>
2020-12-07 00:55:38 +0800

    feat: adding init sql

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA public
+    AUTHORIZATION postgres;
+
+COMMENT ON SCHEMA public
+    IS 'standard public schema';
+
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+GRANT ALL ON SCHEMA public TO postgres;
